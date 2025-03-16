cc84dfe40a28e08baa02762e884897ba9280b996
Cody Potter <me@codypotter.com>
2025-03-16 12:23:28 -0400

    Upgrades to Angular 19 (#1)
    * upgraded core and cli to 18
    
    * updates typescript to 5.5
    
    * upgrades fontawesome
    
    * upgrades @angular/core@19 @angular/cli@19
    
    * Deploy version 0.0.6
    
    * removes particle effects for clarity
    
    * adds github workflow for deploy
    
    * Revert "Deploy version 0.0.6"
    
    This reverts commit edf715e64bfb6175fd454e22008048a541ddd717.
    
    ---------
    
    Co-authored-by: Cody Potter <cody.potter@gearbox.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,23 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { NgxParticlesModule, NgParticlesService } from '@tsparticles/angular';
-import { IOptions, RecursivePartial } from '@tsparticles/engine';
-import { loadSlim } from '@tsparticles/slim';
 import { version } from '../../package.json';
-import { ParticlesConfigService } from './particles-config.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, FontAwesomeModule, NgxParticlesModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    imports: [CommonModule, FontAwesomeModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'landing-page';
 
   faGithub = faGithub;
   faLinkedin = faLinkedin;
   faYoutube = faYoutube;
 
-  particlesOptions: RecursivePartial<IOptions>;
-
-  constructor(
-    private particlesConfigService: ParticlesConfigService,
-    private readonly ngParticlesService: NgParticlesService,
-  ) {
+  constructor() {
     console.trace('AppComponent constructor with version', version);
-    this.particlesOptions = this.particlesConfigService.getConfig();
-  }
-
-  ngAfterViewInit(): void {
-    console.trace('AppComponent ngAfterViewInit');
-    this.ngParticlesService.init(async (engine) => {
-      console.trace('engine', engine);
-      await loadSlim(engine).then(() => {
-        const particlesElement = document.querySelector('.particles') as HTMLElement;
-        if (particlesElement) {
-          particlesElement.style.display = 'block';
-        }
-      }).catch((e) => console.error(e));
-    })
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,8 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { NgParticlesService, NgxParticlesModule } from '@tsparticles/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(NgxParticlesModule, BrowserAnimationsModule),
-    NgParticlesService,
+    importProvidersFrom(BrowserAnimationsModule),
   ]
 };
