5b07076057cd6774f4eed808f21d0e0c934e81e1
Cody Potter <cody.potter@gearbox.com>
2025-04-26 19:18:04 -0400

    removes angular

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-
-console.warn(`Application is starting`);
-
-bootstrapApplication(AppComponent, appConfig)
-  .then(() => console.warn('Application is running'))
-  .catch((err) => console.error(err));
